1b06512d770d358b8c11d96a1fbe9d6661603936
pissulin <pissulin.ads@gmail.com>
2020-10-08 21:36:25 -0300

    logo e header finish

---
+++ src/components/template/Main.jsx
@@ -0,0 +1,11 @@
+import './Main.css'
+import React, {Fragment} from 'react'
+import Header from "./Header"
+
+export default props =>
+    <Fragment>
+        <Header {...props}/>
+        <main className="content">
+            Conteúdo
+        </main>
+    </Fragment>
